daf215d5d5161326464e36600bbb660cdf12c938
mildapo <mildapo@gmail.com>
2026-06-10 18:55:37 +0200

    Implement PWA support and Teacher CSV export

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'klem-2099-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './game-data.js',
+  './qrcode.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response.ok) {
+          const resClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
